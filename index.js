6c1e0428045c8851a1925a257948da06b7d82470
Arash <arashkaffashi@gmail.com>
2022-06-20 20:43:40 -0400

    Implement listeners

--- index.js
+++ index.js
@@ -5,4 +5,47 @@ const operators = {
 	divite: (a, b) => a / b,
 };
 
+let state = {
+	previousButton: null,
+	operations: [],
+};
+
 function operate(operator, a, b) {}
+
+function handleOperand(e) {
+	let op = e.target.textContent;
+
+	switch (op) {
+		case "C":
+			break;
+		case "/":
+			break;
+		case "*":
+			break;
+		case "-":
+			break;
+		case "+":
+			state.operations.push("+");
+			break;
+		case "=":
+			break;
+		case ".":
+			break;
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "0":
+			console.log(+op);
+			break;
+	}
+}
+
+Array.from(document.querySelectorAll(".button")).forEach((button) => {
+	button.addEventListener("click", handleOperand);
+});
